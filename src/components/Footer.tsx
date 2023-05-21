746d833d9b56c689ca332269837d4dc0316ebeee
Vianney Accart <vianneyaccart@gmail.com>
2023-05-21 13:52:28 +0200

    Add aria label on footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,22 @@ import { GITHUB_URL, LINKEDIN_URL, TWITTER_URL } from "@/links";
 import Legals from "./Legals";
 import { useState } from "react";
 
-const FooterLink = ({ icon: Icon, url }: { icon: IconType; url: string }) => {
+const FooterLink = ({
+  icon: Icon,
+  url,
+  ariaLabel,
+}: {
+  icon: IconType;
+  url: string;
+  ariaLabel: string;
+}) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={ariaLabel}
+    >
       <div className="nav-icon w-12 h-12 border border-solid border-1 border-neutral-500 dark:border-gray-200 rounded-full flex items-center justify-center">
         <Icon className="text-md" role="button" />
       </div>
@@ -30,9 +43,21 @@ const Footer = () => {
   return (
     <footer className="px-5 mt-48 flex flex-col items-center">
       <div className="flex gap-7">
-        <FooterLink icon={FiGithub} url={GITHUB_URL} />
-        <FooterLink icon={BsTwitter} url={TWITTER_URL} />
-        <FooterLink icon={FaLinkedinIn} url={LINKEDIN_URL} />
+        <FooterLink
+          icon={FiGithub}
+          url={GITHUB_URL}
+          ariaLabel="Visit Vianney's GitHub"
+        />
+        <FooterLink
+          icon={BsTwitter}
+          url={TWITTER_URL}
+          ariaLabel="Visit Vianney's Twitter"
+        />
+        <FooterLink
+          icon={FaLinkedinIn}
+          url={LINKEDIN_URL}
+          ariaLabel="Visit Vianney's LinkedIn"
+        />
       </div>
       <div className="mb-12 mt-4">
         <p className="mt-4 font-light text-lg text-center">
